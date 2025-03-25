f4332ff89c869f18cdd2d264d0b7423270484463
Mesan0477 <mertsinanbasaran08@gmail.com>
2025-03-25 20:52:40 +0300

    Form1.cs

---
+++ Login/Aqua Login/Form1.cs
@@ -0,0 +1,16 @@
+//Please turn off your translation.
+namespace YourNamespace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Logined Successfully", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
